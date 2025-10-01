45a32c186960e776c8fc7b8a7eb34408a3637eb6
Justin Kim <mletemps@hotmail.com>
2025-09-30 23:27:37 -0400

    added shadcn components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+import { Button } from '@/components/ui/button'
+
 export default function Home() {
-    return <main className="">HomePage</main>
+    return (
+        <main className="bg-primary">
+            HomePage
+            <div>
+                <Button variant="default">Button</Button>
+            </div>
+        </main>
+    )
 }
