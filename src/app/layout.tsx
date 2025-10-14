86e938bbcce49765c2bb9e6447f3d565d40c8397
Justin K <mletemps@hotmail.com>
2025-10-14 00:18:56 -0400

    added toaster to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import { Navbar } from '@/components/Navbar'
 import { ThemeProvider } from '@/components/theme-provider'
-import './globals.css'
+import { Toaster } from '@/components/ui/sonner'
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
+import './globals.css'
 
 const geistSans = Geist({
     variable: '--font-geist-sans',
@@ -38,6 +39,7 @@ export default function RootLayout({
                 >
                     <Navbar />
                     {children}
+                    <Toaster />
                 </ThemeProvider>
             </body>
         </html>
